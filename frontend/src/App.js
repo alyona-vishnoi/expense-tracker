527e0fc91dcecd6ffe4e3af51220d3f9e3e096af
alyona-vishnoi <88257366+alyona-vishnoi@users.noreply.github.com>
2023-05-27 22:42:26 -0400

    Add new folder and files

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+import React, {useState, useMemo} from 'react'
+import styled from "styled-components";
+import bg from './img/bg.png'
+import {MainLayout} from './styles/Layouts.js'
+import Navigation from './Components/Navigation/Navigation'
+import Dashboard from './Components/Dashboard/Dashboard';
+import Income from './Components/Income/Income'
+import Expenses from './Components/Expenses/Expenses';
+import { useGlobalContext } from './context/globalContext';
+
+/*A switch statement is a control flow statement in programming languages
+ that allows you to select and execute different sections of code based on
+ the value of a given expression or variable. It provides a concise way 
+ to write multiple conditional statements without the need for numerous if-else statements.*/
+
+function App() {
+  const [active, setActive] = useState(1)
+  const global = useGlobalContext()
+  console.log(global);
+
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
+  return (
+    <AppStyled bg={bg} className="App">
+      <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+        <main>
+        {displayData()}
+        </main>
+      </MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
+
+export default App;
